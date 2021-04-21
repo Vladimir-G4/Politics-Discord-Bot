66462edc0e24fba6c1875cb1e423cf9d9c079307
Vladimir <vg377@njit.edu>
2021-04-21 15:44:16 -0400

    Update automate.py

--- automate.py
+++ automate.py
@@ -1,8 +1,4 @@
 from DiscordBot import autoUpdate
-import asyncio
-import sys
+import trio
 
-if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
-    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
-asyncio.run(autoUpdate(781536635481096213))
+trio.run(autoUpdate(781536635481096213))
