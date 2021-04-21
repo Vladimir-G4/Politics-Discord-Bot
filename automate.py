43eb1d90437b457f432201e03487d133ba7d14da
Vladimir <vg377@njit.edu>
2021-04-21 19:37:16 -0400

    Update automate.py

--- automate.py
+++ automate.py
@@ -1,4 +1,5 @@
 from DiscordBot import autoUpdate
 import asyncio
 
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 asyncio.get_event_loop().run_until_complete(autoUpdate())
