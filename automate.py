9ab246ca2fc3877b9f9360215ffc435a58583bc9
Vladimir <vg377@njit.edu>
2021-04-21 15:40:57 -0400

    Update automate.py

--- automate.py
+++ automate.py
@@ -1,4 +1,8 @@
 from DiscordBot import autoUpdate
 import asyncio
+import sys
+
+if sys.version_info[0] == 3 and sys.version_info[1] >= 8 and sys.platform.startswith('win'):
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 asyncio.run(autoUpdate(781536635481096213))
