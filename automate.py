95124d239b4f47cfd846db56953626397be228ec
Vladimir <vg377@njit.edu>
2021-04-21 17:30:44 -0400

    Update automate.py

--- automate.py
+++ automate.py
@@ -2,5 +2,5 @@ from DiscordBot import autoUpdate
 import asyncio
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(asyncio.ensure_future(autoUpdate))
+loop.run_until_complete(asyncio.ensure_future(autoUpdate()))
 loop.run_until_complete(asyncio.sleep(1))
