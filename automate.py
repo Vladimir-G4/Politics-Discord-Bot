83972418bf82569992b54459176c880b83079c70
Vladimir <vg377@njit.edu>
2021-04-21 23:10:48 -0400

    Update automate.py

--- automate.py
+++ automate.py
@@ -3,5 +3,5 @@ import time
 import asyncio
 
 time.sleep(4)
-asyncio.set_event_loop(asyncio.ProactorEventLoop())
-asyncio.get_event_loop().run_until_complete(autoUpdate())
+loop = asyncio.new_event_loop()
+loop.run_until_complete(autoUpdate())
