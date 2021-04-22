306ccc441c43be09d130ccf406c4f1882fd26eed
Vladimir <vg377@njit.edu>
2021-04-21 20:00:21 -0400

    Update automate.py

--- automate.py
+++ automate.py
@@ -3,6 +3,4 @@ import time
 import asyncio
 
 time.sleep(4)
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-time.sleep(2)
 asyncio.get_event_loop().run_until_complete(autoUpdate())
